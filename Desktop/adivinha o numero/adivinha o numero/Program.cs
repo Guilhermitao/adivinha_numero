8c47d9d808f1a5daa4c97c2911b05fd0e6325275
guilherme leandro gomes oliveira <guig.oliver@hotmail.com>
2022-06-23 21:46:38 -0300

    cu

---
+++ Desktop/adivinha o numero/adivinha o numero/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace adivinha_o_numero
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double valor = 0;
+
+            while (valor != 666)
+            {
+                Console.WriteLine("jogo de adivinhação.qual é o numero?\r\nboa sorte procurando");
+                valor = double.Parse(Console.ReadLine());
+                if (valor != 666)
+                {
+                    Console.WriteLine("Errou,tente novamente.");
+                }
+            }//while
+            Console.WriteLine("parabens,acertou o numero");
+            Console.ReadKey();
+        }//static void
+    }//class program
+}//namespace
